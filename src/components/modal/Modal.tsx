c3ecd7bfc54921d5a694139d729313810bc07357
Gustav Syrén <g.e.syren@gmail.com>
2024-05-13 14:50:19 +0200

    review validation

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -7,6 +7,9 @@ import { ModalProps } from "../../types/types";
 
 const Modal: React.FC<ModalProps> = ({ onClose }) => {
   const { dispatch } = useContext(GlobalContext);
+  const [pageNumError, setPageNumError] = useState(false)
+  const [reviewError, setReviewError] = useState(false)
+  const [scoreError, setScoreError] = useState(false)
   const parsedVersion = useVersion();
   const [formData, setFormData] = useState({
     scoreValue: "0",
@@ -16,6 +19,21 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
   });
 
   const handleClick = () => {
+    const scoreValue = parseInt(formData.scoreValue);
+    const numPageValue = parseInt(formData.numPageValue);
+    const reviewLength = formData.review.length;
+    const scoreError = scoreValue < 1 || scoreValue > 5;
+    const pageNumError = numPageValue < 1;
+    const reviewError = reviewLength < 10;
+  
+    setScoreError(scoreError);
+    setPageNumError(pageNumError);
+    setReviewError(reviewError);
+  
+    if (scoreError || pageNumError || reviewError) {
+      return;
+    }
+  
     dispatch({ type: "SAVE_REVIEW", payload: formData });
     onClose();
   };
@@ -44,6 +62,7 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
             maxValue={5}
             name="scoreValue"
           ></Input>
+          {scoreError && <p className="input-error">Score needs to be between 1 and 5</p>}
         </div>
         <div>
           Number of Pages:{" "}
@@ -54,6 +73,7 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
             className="input modal"
             name="numPageValue"
           ></Input>
+          {pageNumError && <p className="input-error">The number of pages needs to be greater than 0</p>}
         </div>
         <div>
           Review: <br />
@@ -64,6 +84,7 @@ const Modal: React.FC<ModalProps> = ({ onClose }) => {
             onChange={handleChange}
             placeholder="Leave your review here!"
           ></textarea>{" "}
+          {reviewError && <p className="input-error">The review needs to be longer than 10 characters</p>}
         </div>
         <div>
           <Button
