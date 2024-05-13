b6c5540306de36aab2fea423be5d73269a3b72be
Gustav Syrén <g.e.syren@gmail.com>
2024-05-13 10:16:20 +0200

    removal of unused logic

--- src/custom-hooks/useFetch.ts
+++ src/custom-hooks/useFetch.ts
@@ -7,25 +7,22 @@ export default function useFetch(
   options: string
 ) {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
   const url = `${endpoint}${query}${options}`;
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
+ 
       try {
         const response = await axios.get(url);
         setData(response.data);
       } catch (error) {
         const axiosError = error as AxiosError;
         console.error("Axios Error:", axiosError.message);
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchData();
   }, [url]);
 
-  return { data, loading };
+  return { data };
 }
